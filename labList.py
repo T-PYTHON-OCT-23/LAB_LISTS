675f7a419eacb9efa28ba64262da1a71c683dcbc
khamis_dossary <khamis.dossary@gmail.com>
2023-10-15 17:39:53 +0300

    Bonus

--- labList.py
+++ labList.py
@@ -24,3 +24,23 @@ newList=oddNums[:5]
 print(newList)
 
 #Bounus
+def bestMovies(_list:list):
+    avarageRating:float=0.00
+    for i in _list:
+        ratingSum=0
+        for j in i[2]:
+            ratingSum+=j
+        avarageRating=round(float(ratingSum)/len(i[2]), 2)
+        if avarageRating>=6:
+            print(f"{i[0]} ({i[1]}) - Avergae rating: {avarageRating} ★")
+
+testList=[
+    ("The Shawshank Redemption", 1994, [9, 10, 10, 9, 8, 9]),
+    ("The Godfather", 1972, [10, 9, 8, 10, 9, 7]),
+    ("Pulp Fiction", 1994, [9, 8, 7, 8, 6, 5]),
+    ("The Dark Knight", 2008, [10, 9, 9, 8, 9, 8]),
+    ("Schindler's List", 1993, [8, 9, 9, 7, 6, 8]),
+    ("The Room", 2003, [1, 2, 3, 4, 5, 1])
+]
+
+bestMovies(testList)
