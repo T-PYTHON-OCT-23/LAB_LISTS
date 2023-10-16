7ff54df64231d2572847cfe2894725dd3adbf7d4
hadeel.i7 <hadeel.naji175@gmail.com>
2023-10-16 07:43:31 +0300

    done

---
+++ list.py
@@ -0,0 +1,36 @@
+#1
+sum_list = list()
+
+items = [5 , 7, 3]
+print(items)
+
+new_list = 5 + 7 + 3
+print(new_list)
+
+#2
+largest_list = (list)
+def largest_number(numbers):
+
+    largest = max(numbers)
+    return largest
+
+numbers_list = [45, 9, 16, 52]
+largest_number = largest_number(numbers_list)
+print("The largest number in the list is:", largest_number)
+
+#3
+odd_list = (list)
+odd_numbers = [num for num in range(1200, 2000, 125) if num % 2 != 0]
+
+print(odd_numbers)
+#4
+new_slice = items[1:3]
+new_identical_list = items[:]
+print(new_slice)
+print(new_identical_list)
+
+
+
+
+
+
