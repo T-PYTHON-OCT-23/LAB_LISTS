4f028e5f240431eb6c1ec47ea3b64a5fd99c9317
MaramAlasmari <147453012+MaramAlasmari@users.noreply.github.com>
2023-10-16 07:51:45 +0300

    done

---
+++ list.py
@@ -0,0 +1,27 @@
+num=[2, 3, 4, 5, 15, 1, 43, 20]
+
+def s(list_num:list): 
+    return sum(list_num)
+
+print(s(num))
+
+def m(list_num:list):
+    return max(list_num)
+
+print(m(num))
+
+
+l3=[]
+for n in range(1200,2000,125):
+    l3.append(n)
+print(l3)
+
+newl3=l3[5]
+print(newl3)
+
+
+
+
+
+
+
