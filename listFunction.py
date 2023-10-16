f2d403a0b15d0f6bd2326f38913867db7b7324c2
Reef <reef.zayed2@gmail.com>
2023-10-16 22:29:38 +0300

    done

--- listFunction.py
+++ listFunction.py
@@ -1,3 +1,5 @@
+
+genralList=[2, 3, 4, 5, 15, 1, 43, 20]
 #Q1
 def sum_list(items):
     sum_numbers = 0
@@ -5,7 +7,7 @@ def sum_list(items):
         sum_numbers += x
     return sum_numbers
 
-print(sum_list([1,2,4]))
+print(sum_list(genralList))
 #Q2
 def largest_list(list):
     max=list[0]
@@ -14,13 +16,15 @@ def largest_list(list):
             max=a
     return max
 
-print(largest_list([1,9,7,3]))
+print(largest_list(genralList))
+
 #Q3
 numberOdd=[]
 for i in range(1200,2000,125):
-    if (i%2!=0):
+    if (i%2!=0):#i%2==1 
         numberOdd.append(i)
-print (numberOdd)
+print(numberOdd)
+
 #Q4
 new_list=numberOdd[1:3]
 print(new_list)
